3bf87e37163aab71a96e496c7f685a0859e0c20d
James <james@trueface.ai>
2019-04-03 18:09:54 -0700

    message

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -53,6 +53,11 @@ public class Main {
     return "index";
   }
 
+  @RequestMapping("/home")
+  String home() {
+    return "home";
+  }
+
   @RequestMapping("/db")
   String db(Map<String, Object> model) {
     try (Connection connection = dataSource.getConnection()) {
