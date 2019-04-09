e8088d74af006009b053b1c5d2bb805ef857539d
James <james@trueface.ai>
2019-04-09 14:03:05 -0700

    more progress

--- src/main/java/com/example/WebServices.java
+++ src/main/java/com/example/WebServices.java
@@ -1,7 +1,14 @@
 package com.example;
 
 //http://square.github.io/okhttp/
+import java.io.IOException;
+
+import com.squareup.okhttp3;
 
 public class WebServices {
+	
+	public static void getJson() {
+		return;
+	}
 
 }
